9f698ea491163e2540077092fde6e581c106b594
Ignacio Nieto <contact@digitalleaves.com>
2017-04-27 15:23:12 +0200

    Initial Commit

---
+++ AutoLayoutAnimations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutAnimations
+//
+//  Created by Ignacio Nieto Carvajal on 27/4/17.
+//  Copyright © 2017 Digital Leaves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
